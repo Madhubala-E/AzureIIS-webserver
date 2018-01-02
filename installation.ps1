01bd1a4cacf164d42477fa82d77e6334b059c0c8
Madhubala-E <31853326+Madhubala-E@users.noreply.github.com>
2018-01-02 12:18:15 +0530

    Azure-Ubuntu-DHCP,DNS

--- installation.ps1
+++ installation.ps1
@@ -7,7 +7,7 @@ Add-WindowsFeature -Name DNS -IncludeManagementTools
 Get-WindowsFeature -Name *DNS*
 
 <# Adding DNS server primary zone #>
-Add-DnsServerPriamryZone -Name zirrusgavs.com -zonefile zirrusgavs.com.DNS -DynamicUpdate NonsecureAndSecure
+Add-DnsServerPrimaryZone -Name zirrusgavs.com -zonefile zirrusgavs.com.DNS -DynamicUpdate NonsecureAndSecure
 
 <# Install DHCP server role #>
 Install-WindowsFeature -Name DHCP -IncludeManagementTools
@@ -15,7 +15,7 @@ Install-WindowsFeature -Name DHCP -IncludeManagementTools
 Get-NetIPConfiguration
 
 <# Adding DHCP server scope #>
-Add-DhcpServerv4Scope -Name DHCPClient -StartRange 10.10.2.1 -EndRange 10.10.2.5 -SubnetMask 255.255.255.0
+Add-DhcpServerv4Scope -Name DHCPClient -StartRange 10.10.3.1 -EndRange 10.10.3.5 -SubnetMask 255.255.255.0
 
 
 
